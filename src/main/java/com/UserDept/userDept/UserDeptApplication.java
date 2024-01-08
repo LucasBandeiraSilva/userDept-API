1970601ca9f8d2d97a6f752a192ec9aa34500448
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-01-08 14:28:06 -0300

    Project created

---
+++ src/main/java/com/UserDept/userDept/UserDeptApplication.java
@@ -0,0 +1,13 @@
+package com.UserDept.userDept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDeptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UserDept/userDept/UserDeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserDept.userDept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
