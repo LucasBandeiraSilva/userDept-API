0b13aa4508fec01c891f0f6c8f2c18c95c511c7b
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-01-13 21:39:43 -0300

    adding a custom method

--- src/main/java/com/UserDept/userDept/repository/UserRepository.java
+++ src/main/java/com/UserDept/userDept/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.UserDept.userDept.repository;
 import com.UserDept.userDept.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    List<User>findByActiveTrue();
 }
